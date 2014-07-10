0c05fd91d676fdf4f78793ef3f607de093c15e3b
Eduardo-Lopes <edulopes@outlook.com>
2014-07-10 16:19:42 -0300

    parallax corrected cubemap with multiple cubemaps in the scene.

---
+++ Lux Scripts/CubemapInfluence.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CubemapInfluence : MonoBehaviour {
+
+	private LuxEnvProbe[] probes;
+
+	private CubemapManager manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = UnityEngine.Object.FindObjectOfType<CubemapManager>();
+	}
+
+	float coverage(LuxEnvProbe probe)
+	{
+		float dx = Mathf.Abs(probe.transform.position.x - transform.position.x);
+		float dy = Mathf.Abs(probe.transform.position.y - transform.position.y);
+		float dz = Mathf.Abs(probe.transform.position.z - transform.position.z);
+
+		float start_dist = 0.0f;
+
+		float end_dist_x = probe.BoxSize.x/2.0f;
+		float end_dist_y = probe.BoxSize.y/2.0f;
+		float end_dist_z = probe.BoxSize.z/2.0f;
+
+		float influencex = 1.0f-Mathf.Max(0, Mathf.Min(1, (dx - start_dist) / (end_dist_x - start_dist)));
+		float influencey = 1.0f-Mathf.Max(0, Mathf.Min(1, (dy - start_dist) / (end_dist_y - start_dist)));
+		float influencez = 1.0f-Mathf.Max(0, Mathf.Min(1, (dz - start_dist) / (end_dist_z - start_dist)));
+
+		return Mathf.Min(influencex,Mathf.Min(influencey,influencez));
+	}
+
+	float CalculateInfluences(out int probe1, out int probe2)
+	{
+		probe1 = probe2 = 0;
+
+		int influence_count = probes.Length;
+
+		float[] influences = new float[influence_count];
+		//float[] blendfactors = new float[influence_count];
+
+		int [] pos = new int[influence_count];
+
+	    float sumInfluences = 0.0f;
+	    float sumInvInfluences = 0.0f;
+	    float sumFactors = 0.0f;
+
+		for (int k = 0; k < probes.Length; k++)
+	    {
+			influences[k] = coverage(probes[k]);
+			pos[k] = k;
+		}
+
+		Array.Sort<int>(pos,(i,j) =>{
+			return influences[j].CompareTo(influences[i]);
+		});
+
+		if(influence_count >= 2)
+		{
+			influence_count = 2;
+		}
+		else
+		{
+			probe1 = probe2 = pos[0];
+			return 1.0f;
+		}
+
+		for (int k = 0; k < influence_count; k++)
+		{
+			float influence = influences[k];
+
+			sumInfluences += influence;
+	        sumInvInfluences += 1.0f - influence;
+	    }
+
+		for (int k = 0; k < influence_count; k++)
+	    {
+			float influence = influences[pos[k]];
+
+			influences[pos[k]] =
+				influence/sumInfluences;
+//				((1.0f - influence)/sumInvInfluences) * 
+//				 (1.0f - influence/sumInfluences) / 
+//					(influence_count - 1);
+
+			sumFactors += influences[pos[k]];
+	    }
+
+		for (int k = 0; k < influence_count; k++)
+	    {
+			influences[pos[k]] = influences[pos[k]]/sumFactors;
+	    }
+
+		probe1 = pos[0];
+
+		if (influence_count>=1)
+			probe2 = pos[1];
+
+		return influences[pos[0]];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		probes = manager.UpdateProbes(probes, transform);
+
+		if(renderer != null)
+		{
+			int probe1, probe2;
+
+			float influence = CalculateInfluences(out probe1, out probe2);
+
+			int materials = renderer.materials.Length;
+			// Get all materials
+			for (int j = 0; j < materials; j++) {
+				if (renderer.materials[j].HasProperty("_CubemapSize"))
+				{
+					renderer.materials[j].SetVector("_CubemapSize", 
+						new Vector4(probes[probe1].BoxSize.x*0.5f, probes[probe1].BoxSize.y*0.5f, probes[probe1].BoxSize.z*0.5f, 0));
+					renderer.materials[j].SetMatrix("_CubeMatrix_Trans", probes[probe1].BoxMatrix.transpose );
+					renderer.materials[j].SetMatrix("_CubeMatrix_Inv", probes[probe1].BoxMatrix.inverse );
+					renderer.materials[j].SetTexture("_DiffCubeIBL", probes[probe1].DIFFCube );
+					if (probes[probe1].SPECCube != null){
+						renderer.materials[j].SetTexture("_SpecCubeIBL", probes[probe1].SPECCube );
+					}
+
+					Debug.Log(string.Format("{0} {1} {2}",probes[probe1].name,probes[probe2].name,influence));
+
+					renderer.materials[j].SetFloat("_Influence", influence);
+
+					renderer.materials[j].SetVector("_CubemapSize2", 
+						new Vector4(probes[probe2].BoxSize.x*0.5f, probes[probe2].BoxSize.y*0.5f, probes[probe2].BoxSize.z*0.5f, 0));
+					renderer.materials[j].SetMatrix("_CubeMatrix_Trans2", probes[probe2].BoxMatrix.transpose );
+					renderer.materials[j].SetMatrix("_CubeMatrix_Inv2", probes[probe2].BoxMatrix.inverse );
+					renderer.materials[j].SetTexture("_DiffCubeIBL2", probes[probe2].DIFFCube );
+					if (probes[probe2].SPECCube != null){
+						renderer.materials[j].SetTexture("_SpecCubeIBL2", probes[probe2].SPECCube );
+					}
+				}
+			}
+		}
+		else {
+			Debug.Log("does not have a mesh renderer attached to it. It has been removed.");
+		}
+	}
+
+}
---
+++ Lux Scripts/CubemapManager.cs
@@ -0,0 +1,166 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class CubemapManager : MonoBehaviour
+{
+	[SerializeField]
+	[HideInInspector]
+	private List<LuxEnvProbe> dicKeys;
+
+	[SerializeField]
+	[HideInInspector]
+	private List<LuxEnvProbe> dicValues;
+
+	[SerializeField]
+	[HideInInspector]
+	private List<int> dicLastIndex;
+
+	private Dictionary<LuxEnvProbe,LuxEnvProbe[]> linkProbes;
+
+	bool Intersect(LuxEnvProbe probe1, LuxEnvProbe probe2)
+	{
+		Vector3 diff = probe1.transform.position-probe2.transform.position;
+		Vector3 dist = (probe1.BoxSize+probe2.BoxSize)/2.0f;
+
+		return (dist.x >= Mathf.Abs(diff.x) &&
+		    	dist.y >= Mathf.Abs(diff.y) &&
+		        dist.z >= Mathf.Abs(diff.z));
+	}
+
+	public void Calculate()
+	{
+		// Get All probes in scene
+		LuxEnvProbe[] probes = UnityEngine.Object.FindObjectsOfType<LuxEnvProbe>();
+		
+		linkProbes = new Dictionary<LuxEnvProbe, LuxEnvProbe[]>();
+
+		List<LuxEnvProbe>[] neighbors = new List<LuxEnvProbe>[probes.Length];
+		
+		for( int i = 0; i < probes.Length; i++ )
+		{
+			neighbors[i] = new List<LuxEnvProbe>();
+
+			for( int j = 0; j < i; j++ )
+			{
+				if (Intersect(probes[i],probes[j]))
+				{
+					neighbors[i].Add(probes[j]);
+					neighbors[j].Add(probes[i]);
+				}
+			}
+		}
+
+		for( int i = 0; i < probes.Length; i++ )
+		{
+			linkProbes.Add(probes[i], neighbors[i].ToArray());
+		}
+
+		dicKeys = new List<LuxEnvProbe>();
+		foreach(var key in linkProbes.Keys)
+			dicKeys.Add(key);
+
+		int pos = 0;
+
+		dicLastIndex = new List<int>();
+		dicValues = new List<LuxEnvProbe>();
+		foreach(var value in linkProbes.Values)
+		{
+			dicValues.AddRange(value);
+			pos += value.Length;
+			dicLastIndex.Add(pos);
+		}
+	}
+
+	void Awake()
+	{
+		linkProbes = new Dictionary<LuxEnvProbe, LuxEnvProbe[]>();
+
+		int pos = 0;
+
+		for( int i = 0; i < dicKeys.Count; i++ )
+		{
+			List<LuxEnvProbe> values = new List<LuxEnvProbe>();
+
+			while(pos != dicLastIndex[i])
+			{
+				values.Add(dicValues[pos]);
+				pos++;
+			}
+
+			linkProbes.Add(dicKeys[i], values.ToArray());
+		}
+	}
+
+	void Start()
+	{
+		//Calculate();
+	}
+
+	void Update()
+	{
+
+	}
+
+	private LuxEnvProbe[] FilterProbes(LuxEnvProbe[] linkProbes, Transform elemTransform)
+	{
+		List<LuxEnvProbe> returnProbes = new List<LuxEnvProbe>();
+
+		foreach(var linkProbe in linkProbes)
+		{
+			Vector3 diff = linkProbe.transform.position-elemTransform.position;
+			Vector3 dist = linkProbe.BoxSize/2.0f;
+			
+			if (dist.x >= Mathf.Abs(diff.x) &&
+			    dist.y >= Mathf.Abs(diff.y) &&
+			    dist.z >= Mathf.Abs(diff.z))
+			{
+				returnProbes.Add(linkProbe);
+			}
+		}
+
+		return returnProbes.ToArray();
+	}
+
+	public LuxEnvProbe[] UpdateProbes(LuxEnvProbe[] currProbes, Transform elemTransform)
+	{
+		List<LuxEnvProbe> returnProbes = new List<LuxEnvProbe>();
+
+		if (currProbes != null && currProbes.Length > 0)
+		{
+			// if already have a list
+			// build with probes already selected
+			// and theirs neighbours
+			// to be filtered
+			returnProbes.AddRange(currProbes);
+
+			foreach(var p in currProbes)
+				foreach(var p2 in linkProbes[p])
+					if (!returnProbes.Contains(p2))
+						returnProbes.Add(p2);
+		}
+		else
+		{
+			// build probe list, add all probes to filter
+			foreach(var probe in linkProbes.Keys)
+				returnProbes.Add(probe);
+		}
+
+		return FilterProbes(returnProbes.ToArray(),elemTransform);
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.black;
+
+		if(linkProbes!=null)
+		{
+			foreach(var probe in linkProbes.Keys)
+			{
+				foreach(var neighbor in linkProbes[probe])
+					Gizmos.DrawLine(
+						probe.transform.position,
+						neighbor.transform.position);
+			}
+		}
+	}
+}
